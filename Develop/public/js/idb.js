d191b1942dca9a71785a7d6ab1840f96c534ef39
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-04-17 17:16:21 -0400

    idb.js

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -13,9 +13,14 @@ request.onupgradeneeded = function(cvent) {
 request.onsuccess = function(event) {
   db = event.target.result;
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
+   
     uploadbank();
   }
+};
+
+request.onerror = function(event) {
+  // log error here
+  console.log(event.target.errorCode);
 };
 
 // This function will be executed if we attempt to submit a new bank and there's no internet connection
@@ -29,3 +34,45 @@ function saveRecord(record) {
   // add record to your store with add method
   bankObjectStore.add(record);
 }
+
+function uploadBank() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(['new_bank'], 'readwrite');
+
+  // access your pending object store
+  const bankObjectStore = transaction.objectStore('new_bank');
+
+  // get all records from store and set to a variable
+  const getAll = bankObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transactions', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_bank'], 'readwrite');
+          const bankObjectStore = transaction.objectStore('new_bank');
+          // clear all items in your store
+          bankObjectStore.clear();
+        })
+        .catch(err => {
+          // set reference to redirect back here
+          console.log(err);
+        });
+    }
+  };
+}
+// listen for app coming back online
+window.assEventListener('online', uploadBank);
